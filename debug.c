4596972ed86e0085fe1a319de65ad47464fcd818
MElevy <deathmoonlevy@gmail.com>
2022-12-23 13:57:10 -0800

    Bug fix

--- debug.c
+++ debug.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "debug.h"
 
-void dissasembleChunk(Chunk* chunk const char* name) {
+void dissasembleChunk(Chunk* chunk, const char* name) {
   printf("=== %s ===\n", name);
 
   for (int offset = 0; offset < chunk->count;) {
